3676a575ddd0f8ebecb7bcd51c1cba60ee8916d6
Iamneo <techadmin@iamneo.ai>
2023-08-05 06:43:31 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -0,0 +1,24 @@
+<dependencies>
+    <!-- Spring Framework -->
+    <dependency>
+        <groupId>org.springframework</groupId>
+        <artifactId>spring-context</artifactId>
+        <version>5.3.9</version>
+    </dependency>
+
+    <!-- TestNG -->
+    <dependency>
+        <groupId>org.testng</groupId>
+        <artifactId>testng</artifactId>
+        <version>7.4.0</version>
+        <scope>test</scope>
+    </dependency>
+
+    <!-- Selenium -->
+    <dependency>
+        <groupId>org.seleniumhq.selenium</groupId>
+        <artifactId>selenium-java</artifactId>
+        <version>3.141.59</version>
+    </dependency>
+</dependencies>
+
